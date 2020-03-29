fb42fbad2d05ebb49e8b807b2d9a415e404f8b81
Adrian Vivas <medcannapo@gmail.com>
2020-03-29 17:36:12 -0500

    Fixed environments

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + './dist/angular-crud-firebase'));
+app.use(express.static(__dirname + '/dist/angular-crud-firebase'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
