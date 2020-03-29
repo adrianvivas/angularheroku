c2e3833f4245c6661b36ea6d1d2e729b53538660
Adrian Vivas <medcannapo@gmail.com>
2020-03-29 17:26:27 -0500

    Fixed environments for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB88lxPhuZNRT-6gEjRMW0xAdWqZVepFdQ",
+    authDomain: "adrian-crud-firebase.firebaseapp.com",
+    databaseURL: "https://adrian-crud-firebase.firebaseio.com",
+    projectId: "adrian-crud-firebase",
+    storageBucket: "adrian-crud-firebase.appspot.com",
+    messagingSenderId: "373820738717",
+    appId: "1:373820738717:web:639f14f8f2461e46c05874"
+  }
 };
